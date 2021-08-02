fdbbe00055aa29142c5df71c3bd6b187c294d607
Seb Seager <seb.seager@gmail.com>
2021-08-02 18:00:41 -0400

    Added start_jupyter_slurm

---
+++ sh/start_jupyter_slurm.sh
@@ -0,0 +1,51 @@
+# based on a script by Christopher JF Cameron
+
+
+# create log directory
+mkdir -p ~/jupyter/logs
+
+# create Jupyter notebook environment
+virtualenv ~/venv_jupyter_server --python=python3  # via virtualenv
+# conda create -yn jupyter_env anaconda python=3  # via conda
+
+# create SLURM job sumission to run Jupyter server
+# https://docs.ycrc.yale.edu/clusters-at-yale/guides/jupyter/
+activate=`which activate`
+home_dir=`readlink -f ~`
+
+# contents of jupyter_server.sh
+echo "#!/bin/sh
+#SBATCH --partition pi_gerstein
+#SBATCH --nodes 1
+#SBATCH --ntasks-per-node 1
+#SBATCH --mem-per-cpu 5G
+#SBATCH --time 1-0:00:00
+#SBATCH --job-name jupyter-notebook
+#SBATCH --output ${home_dir}/jupyter/logs/jupyter-notebook-%J.log
+
+# get tunneling info
+XDG_RUNTIME_DIR=\"\"
+port=\$(shuf -i8000-9999 -n1)
+node=\$(hostname -s)
+user=\$(whoami)
+cluster=\$(hostname -f | awk -F'.' '{print \$2}')
+
+# print tunneling instructions jupyter-log
+echo -e \"
+For more info and how to connect from Windows, see https://docs.ycrc.yale.edu/clusters-at-yale/guides/jupyter/
+On macOS or Linux, create ssh tunnel with
+ssh -N -L \${port}:\${node}:\${port} \${user}@\${cluster}.hpc.yale.edu
+Use a Browser on your local machine to go to:
+localhost:\${port}  (prefix w/ https:// if using password)
+\"
+
+source $activate jupyter_env
+jupyter-notebook --no-browser --port=\${port} --ip=\${node}
+" > ~/jupyter/jupyter_server.sh
+
+# submit SLURM job
+rm -f ~/jupyter/logs/*.log
+sbatch ~/jupyter/jupyter_server.sh
+
+# get tunnel commands
+ls -d ~/jupyter/logs/*.log | tail -n 1 | xargs -n 1 -I {} cat {}
